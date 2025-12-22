e30748aa0a82d988040d457672f55480b7221c7c
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2025-12-22 01:19:08 +0100

    fix: use staging by default

--- lib/const.ts
+++ lib/const.ts
@@ -1,4 +1,6 @@
 import "dotenv/config";
 
-export const API_URL = process.env.API_URL || "https://cloud.flowershow.app";
-export const APP_URL = process.env.APP_URL || "https://my.flowershow.app";
+export const API_URL =
+  process.env.API_URL || "https://staging-cloud.flowershow.app";
+export const APP_URL =
+  process.env.APP_URL || "https://staging-my.flowershow.app";
